7cbef28d76502536b78916ec441ada3911873cd3
Yuri Padilha <yuri.m.padilha@gmail.com>
2019-04-21 11:19:51 -0300

    fixing div to li and creating state for the app

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Product } from '../Product';
+import { Product, ProductProps } from '../Product';
 
-export class App extends Component {
+interface AppProps {}
+
+interface AppState {
+  products: ProductProps[];
+}
+
+export class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+
+    this.state = {
+      products: [
+        { name: 'White t-shirt', price: 30.2 },
+        { name: 'Red socks', price: 10.7 },
+        { name: 'Brown trousers', price: 55.1 },
+        { name: 'Black jacket', price: 155.9 },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="app">
@@ -10,9 +29,9 @@ export class App extends Component {
         <main className="main container">
           <h1>Products</h1>
           <ol>
-            <Product name="White t-shirt" price={30.2} />
-            <Product name="Red socks" price={10.7} />
-            <Product name="Brown trousers" price={55.1} />
+            {this.state.products.map(({ name, price }) => (
+              <Product name={name} price={price} />
+            ))}
           </ol>
         </main>
       </div>
--- src/Product/Product.tsx
+++ src/Product/Product.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 
-interface ProductProps {
+export interface ProductProps {
   name: string;
   price: number;
 }
 
 export const Product: React.FC<ProductProps> = ({ name, price }) => {
   return (
-    <div className="product">
+    <li className="product">
       <h3 className="product-name">{name}</h3>
       <span className="product-price">{price}</span>
-    </div>
+    </li>
   );
 };
